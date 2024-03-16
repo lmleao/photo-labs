d5bfbebfca538e516febc774f122c4b38fec3e3b
Luiza Leao <luiza.leao96@gmail.com>
2024-03-16 19:51:20 -0400

    Add styling to component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,18 +6,22 @@ const PhotoListItem = (props) => {
   const { photoData } = props;
 
   return (
-    <div className="photo-list-item">
-      <div className="photo">
+    <div className="photo-list__item">
+      <div className="photo-list__image">
         <img src={photoData.imageSource} alt="Photo" />
       </div>
-      <div className="details">
-        <div className="user">
-          <img src={photoData.profile} alt="User" />
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={photoData.profile}
+          alt="User"
+        />
+        <div className="photo-list__user-info">
           <span>{photoData.username}</span>
-        </div>
-        <div className="location">
-          <span>{photoData.location.city},</span>
-          <span>{photoData.location.country}</span>
+          <div className="photo-list__user-location">
+            <span>{photoData.location.city},</span>
+            <span>{photoData.location.country}</span>
+          </div>
         </div>
       </div>
     </div>
