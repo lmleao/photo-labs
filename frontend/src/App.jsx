993df75b36f9ebc4bfb36fb829164b57c578be37
Luiza Leao <luiza.leao96@gmail.com>
2024-03-17 03:50:52 -0400

    Create TopNavigation component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 //import PhotoListItem from './components/PhotoListItem';
 import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList'; 
+import TopicList from './components/TopicList';
+import TopNavigationBar from './components/TopNavigationBar';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <div className="app">
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <TopNavigationBar />
     <PhotoList />
     <TopicList />
     </div>
