ec79abae719122b6a3b16530e7b943c2b6da810b
Luiza Leao <luiza.leao96@gmail.com>
2024-03-17 04:13:55 -0400

    Create HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
-//import PhotoListItem from './components/PhotoListItem';
-import PhotoList from './components/PhotoList';
-import TopicList from './components/TopicList';
-import TopNavigationBar from './components/TopNavigationBar';
+import HomeRoute from './routes/HomeRoute';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="app">
-    {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-    <TopNavigationBar />
-    <PhotoList />
+    <HomeRoute />
     </div>
   );
 };
