f6e406d556b557f2f430aafc646f8cae58e4d7e0
Luiza Leao <luiza.leao96@gmail.com>
2024-03-17 03:52:39 -0400

    Remove TopicList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,6 @@ const App = () => {
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
     <TopNavigationBar />
     <PhotoList />
-    <TopicList />
     </div>
   );
 };
