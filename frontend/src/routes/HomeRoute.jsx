5ee277833bbaa69f547f98f17247f6e373a6880b
Luiza Leao <luiza.leao96@gmail.com>
2024-03-17 05:28:47 -0400

    Store liked photos

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TopNavigation from '../components/TopNavigationBar';
 import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = ({ photos, topics }) => {
+  const [favoritePhotos, setFavoritePhotos] = useState([]);
+
+  const addFavoritePhoto = (photoId) => {
+    if (!favoritePhotos.includes(photoId)) {
+      setFavoritePhotos([...favoritePhotos, photoId]);
+    }
+  };
+
+  const removeFavoritePhoto = (photoId) => {
+    setFavoritePhotos(favoritePhotos.filter((id) => id !== photoId));
+  };
+
   return (
     <div className="home-route">
-      <TopNavigation topics={topics} />
-      <PhotoList photos={photos} />
+      <TopNavigation topics={topics} favoritePhotos={favoritePhotos} />
+      <PhotoList photos={photos} onAddFavorite={addFavoritePhoto} onRemoveFavorite={removeFavoritePhoto} />
     </div>
   );
 };
