44550e45ecf2c9966f228531614ada7da7c7a079
Luiza Leao <luiza.leao96@gmail.com>
2024-03-17 03:24:29 -0400

    Fix title in TopicListItem

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -5,7 +5,7 @@ import "../styles/TopicListItem.scss";
 const TopicListItem = (props) => {
   return (
     <div className="topic-list__item">
-      {props.topicData.label}
+      {props.topicData.title}
     </div>
   );
 };
